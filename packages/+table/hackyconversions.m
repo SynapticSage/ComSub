8c12114f36fe642b75a748855468ad2cdf81ebe4
Ryan <ryoung@brandeis.edu>
2023-08-08 19:05:59 -0400

    hacky conversion fixess

--- packages/+table/hackyconversions.m
+++ packages/+table/hackyconversions.m
@@ -1,11 +1,19 @@
 function tab = hackyconversions(tab)
 % just performs a few hacky conversions on tables for datatype compatability purposes
 
-if ismember('numWindowsCut',fieldnames(tab)) && numel(tab.numWindowsCut(1,:)) > 1
-    tmp = tab.numWindowsCut
-    tmp = num2cell(tab.numWindowsCut,2);
-    tmp = cellfun(@string, tmp, 'UniformOutput', false);
-    tmp = cellfun(@(x)strjoin(x,'-'), tmp, 'UniformOutput', false);
-    tmp = string(tmp);
-    tab.numWindowsCut = tmp;
-end
+tab = stringify(tab, 'numWindowsCut');
+tab = stringify(tab, 'cutoffs');
+tab = stringify(tab, 'lowcutoffs');
+tab = stringify(tab, 'patternNames');
+tab = stringify(tab, 'patternNamesFull');
+
+function tab = stringify(tab, fieldname)
+    if ismember(fieldname,fieldnames(tab)) && numel(tab.(fieldname)(1,:)) > 1
+        disp("stringifying " + fieldname)
+        tmp = tab.(fieldname);
+        tmp = num2cell(tab.(fieldname),2);
+        tmp = cellfun(@string, tmp, 'UniformOutput', false);
+        tmp = cellfun(@(x)strjoin(x,'__'), tmp, 'UniformOutput', false);
+        tmp = string(tmp);
+        tab.(fieldname) = tmp;
+    end
